7928ee6cfc5ec5995dae07aec059badec258b250
Nicolás Fiore <91384707+NicolasFiore@users.noreply.github.com>
2021-11-26 14:30:54 -0300

    Agregado de Nav

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <>
+        <nav>
+            <img src="../img/logo.png" alt="logo" />
+            <h1>Fiore Compain</h1>
+            <ul>
+                <li>Incio</li>
+                <li>Productos</li>
+                <li>Quienes somos</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default NavBar
