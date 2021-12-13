1391af000178cec473fddc865eaec0ee3954b5ea
Nicolás Fiore <91384707+NicolasFiore@users.noreply.github.com>
2021-12-13 00:56:16 -0300

    Agregado de Items clase 6

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import {getFetch} from '../../helpers/getFetch'
 import ItemCount from '../ItemCount/ItemCount'
+import ItemList from '../ItemList/ItemList'
 
 function ItemListContainer({greeting}) {
+    const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        getFetch
+        .then(resp => setProductos(resp.filter(prod => prod.categoria === idCate))) 
+        .catch(err => console.log(err))
+        .finally(()=>setLoading(false))       
+    }, [] )  
+
+
     return (
         <div>
-            <h3>
-                {greeting}
-            </h3>
-            <ItemCount/> 
+            <h3>{greeting}</h3>
+            { loading ? 
+                <h2>Cargando...</h2> 
+                :  
+               <ItemList productos={productos} />
+            }
+
         </div>
     )
 }
